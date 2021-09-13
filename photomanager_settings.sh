89feedac91f09286b4023b98d3131c3a73cf9c1c
Tloz <scrimet@hotmail.fr>
2021-09-14 01:17:28 +0200

    Check function now works properly

--- photomanager_settings.sh
+++ photomanager_settings.sh
@@ -18,6 +18,7 @@ VERBOSITY="s" # s(tdout) | l(og) | q(uiet)
 # log
 MAIN_LOG="photomanager.log"
 CLEAN="no"
+CHECK="no"
 
 # Regex
 REGEX_SEP="[A-Za-z -_]*"
@@ -30,7 +31,7 @@ REGEX_NOTE="[A-Za-z -_]*"
 REGEX_EXT="\.[[:alnum:]]{3,4}"
 
 REGEX_FINAL="^($REGEX_SEP)$REGEX_DATE($REGEX_SEP)($REGEX_TCODE)($REGEX_NOTE)($REGEX_EXT)\$"
-REGEX_TARGET="^[0-9]{4}-[0-9]{2}-[0-9]{2}( [0-9]{4,6})?$REGEX_EXT\$"
+REGEX_TARGET="^[0-9]{4}-[0-9]{2}-[0-9]{2}( [0-9]{4,6}(_[0-9]+)?)?$REGEX_EXT\$"
 ################################
 
 ########## ARGUMENTS ###########
